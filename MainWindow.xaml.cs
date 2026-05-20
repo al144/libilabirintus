7f4ccb1d4a6d3c80886326205d5a7958d8a6e880
al144 <davidasztral2007@gmail.com>
2026-05-20 22:32:39 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace libilabirintus
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Maze maze = new("na");
+
+            int rows = maze.Row;
+            int columns = maze.Column;
+
+            citygrid.RowDefinitions.Clear();
+            citygrid.ColumnDefinitions.Clear();
+            citygrid.Children.Clear();
+
+            for (int i = 0; i < rows; i++)
+            {
+                citygrid.RowDefinitions.Add(new RowDefinition());
+            }
+
+            for (int i = 0; i < columns; i++)
+            {
+                citygrid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < columns; col++)
+                {
+                    if (maze.Map[col, row] == '.')
+                    {
+                        continue;
+                    }
+                    Label label = new Label
+                    {
+                        Content = maze.Map[col, row],
+                        FontSize = 80,
+                    };
+
+                    Grid.SetRow(label, row);
+                    Grid.SetColumn(label, col);
+
+                    citygrid.Children.Add(label);
+                }
+            }
+        }
+    }
+}
---
+++ Maze.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libilabirintus
+{
+    class Maze
+    {
+        public string Name { get; private set; }
+        public int TreasuryNum { get; private set; }
+        public int Row { get; private set; }
+        public int Column { get; private set; }
+        public char[,] Map { get; private set; }
+
+
+        public Maze(string name)
+        {
+            this.Name = name;
+            this.Map = getMap();
+            this.TreasuryNum = getTreasuryRomNum();
+        }
+
+        char[,] getMap()
+        {
+            OpenFileDialog ofd = new();
+            char[,] map;
+
+            if (ofd.ShowDialog() == false)
+            {
+                throw new Exception("nem jo a fajl");
+            }
+
+            string[] notCharMap = File.ReadAllLines(ofd.FileName);
+
+            map = new char[notCharMap[0].Length, notCharMap.Length];
+            this.Row = notCharMap.Length;
+            this.Column = notCharMap[0].Length;
+
+            for (int i = 0; i < notCharMap.Length; i++)
+            {
+                for (int j = 0; j < notCharMap[i].Length; j++)
+                {
+                    map[j, i] = notCharMap[i][j];
+                }
+            }
+
+            return map;
+        }
+
+        int getTreasuryRomNum()
+        {
+            int num = 0;
+
+
+            return num;
+        }
+    }
+}
