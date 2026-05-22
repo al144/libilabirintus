1ec9dd93c047686c7ff853a311bf1e3e30f2666f
al144 <davidasztral2007@gmail.com>
2026-05-23 00:26:28 +0200

    started player

--- Player.cs
+++ Player.cs
@@ -8,5 +8,20 @@ namespace libilabirintus
 {
     class Player
     {
+        public string Name { get; private set; }
+        public List<Maze> PlayedMazes { get; private set; }
+        public List<char[,]> Explored {  get; private set; }
+
+        public Player(string Name)
+        {
+            this.Name = Name;
+            PlayedMazes = new List<Maze>();
+            Explored = new List<char[,]>();
+        }
+
+        public void AlredyPlayed(Maze maze)
+        {
+            PlayedMazes.Add(maze);
+        }
     }
 }
